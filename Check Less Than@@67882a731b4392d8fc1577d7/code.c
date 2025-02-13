089defabf19f9dcd3671863e76653394e8ec1bac
harshita1338 <harshitagarg1102@gmail.com>
2025-02-14 00:49:34 +0530

    Solution to Check Less Than

---
+++ Check Less Than@@67882a731b4392d8fc1577d7/code.c
@@ -0,0 +1,13 @@
+// Your code here...
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+    int x,y;
+    scanf("%d %d",x,y);
+    bool isless=x<y;
+    if(isless){
+        printf("True");
+    }else{
+        printf("False");
+    }
+}
