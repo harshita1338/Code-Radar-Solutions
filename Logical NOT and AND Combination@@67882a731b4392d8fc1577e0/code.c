32351f915c2a0a68a368183533e3e5b49271c34c
harshita1338 <harshitagarg1102@gmail.com>
2025-02-14 11:18:15 +0530

    Solution to Logical NOT and AND Combination

---
+++ Logical NOT and AND Combination@@67882a731b4392d8fc1577e0/code.c
@@ -0,0 +1,15 @@
+// Your code here...
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+    int x,y;
+    scanf("%d %d",&x,&y);
+    bool isgreaterzero=!(x>o && y>0);
+    if (isgreaterzero){
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
+    return 0;
+}
