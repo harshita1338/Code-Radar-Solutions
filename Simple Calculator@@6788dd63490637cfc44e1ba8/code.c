0513115de91e5759fb47d31920e0e5baa88d919d
harshita1338 <harshitagarg1102@gmail.com>
2025-02-17 21:16:12 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -7,17 +7,20 @@ int main(){
     if(ch=='+'){
         printf("%.0f",x+y);
     }
-    else if(ch=="-"){
+    else if(ch=='-'){
         printf("%.0f",x-y);
     }
-    else if(ch=="*"){
+    else if(ch=='*''){
         printf("%.0f",x*y);
     }
-    else if(ch=="/"){
+    else if(y!=0 && ch=='/'){
         printf("%.0f",x/y);
     }
+    else if(y==0 && ch=='/'){
+        printf("error");
+    }
     else{
-        printf("Error");
+        printf("error");
     }
     return 0;
 }
